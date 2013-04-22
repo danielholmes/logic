000d4ee9e9aed5fb0ec46e2eba7d6239ecc55580
= <daniel@creatio.com.au>
2013-04-22 13:21:29 +1000

    Parsing refactoring

--- logic/parser.py
+++ logic/parser.py
@@ -19,9 +19,9 @@ class Parser(object):
     TOKEN_PATTERN = re.compile("\s*(?:([a-z]{1}[a-zA-Z\_0-9]*)|(\-|\^|\||\<\=\>|\=\>|\<\=|\(|\)))")
 
     def __call__(self, program):
-        stream = self.tokenise(program)
-        initial_state = ParseState(stream)
-        if initial_state.token.__class__ == EndToken:
+        raw_tokens = Parser.TOKEN_PATTERN.findall(program)
+        initial_state = ParseState(raw_tokens)
+        if initial_state.is_end:
             raise ParsingError("Empty expression")
 
         return self.expression(initial_state)
@@ -32,72 +32,79 @@ class Parser(object):
         # TODO: Remove
         self.state = current_state
 
-        left = previous_state.token.create_prefixed_expression(self, current_state)
+        prefixed_state = previous_state.token.create_prefixed_expression(self, current_state)
+        left = prefixed_state.left
+
         while right_binding_power < self.state.binding_power:
-            previous_token = self.state.token
+            previous_state = self.state
 
             current_state = previous_state.next(left)
 
             # TODO: Remove
             self.state = current_state
 
-            left = previous_token.create_inside_expression(self, current_state)
+            left = previous_state.token.create_inside_expression(self, current_state)
 
         return left
 
-    def advance(self, state, token_type):
-        if self.state.token.__class__ != token_type:
-            raise ParsingSyntaxError("Expected %r" % token_type.__name__)
-        self.state = state.next()
-
-    def tokenise(self, program):
-        for literal_value, operator in Parser.TOKEN_PATTERN.findall(program):
-            if literal_value:
-                yield ConstantToken(literal_value)
-            elif operator == "-":
-                yield NegationToken()
-            elif operator == "^":
-                yield ConjunctionToken()
-            elif operator == "|":
-                yield DisjunctionToken()
-            elif operator == "<=>":
-                yield EquivalenceToken()
-            elif operator == "=>":
-                yield ImplicationToken()
-            elif operator == "<=":
-                yield ReductionToken()
-            elif operator == "(":
-                yield LeftParenthesisToken()
-            elif operator == ")":
-                yield RightParenthesisToken()
-            else:
-                raise ParsingSyntaxError("Unknown operator to tokenise %s" % operator)
-        yield EndToken()
-
 class ParseState(object):
-    def __init__(self, stream, left = None):
-        self._token = next(stream)
-        self._stream = stream
+    def __init__(self, tokens, left = None):
         self._left = left
+        self._remaining_tokens = tokens[1:]
+
+        parsed_token = None
+        if len(tokens) == 0:
+            self._token = EndToken()
+        else:
+            self._token = self.create_token(tokens[0])
+
+    def create_token(self, raw_token):
+        literal_value, operator = raw_token
+        if literal_value:
+            return ConstantToken(literal_value)
+        elif operator == "-":
+            return NegationToken()
+        elif operator == "^":
+            return ConjunctionToken()
+        elif operator == "|":
+            return DisjunctionToken()
+        elif operator == "<=>":
+            return EquivalenceToken()
+        elif operator == "=>":
+            return ImplicationToken()
+        elif operator == "<=":
+            return ReductionToken()
+        elif operator == "(":
+            return LeftParenthesisToken()
+        elif operator == ")":
+            return RightParenthesisToken()
+        else:
+            raise ParsingSyntaxError("Unknown operator to tokenise %s" % operator)
 
     @property
     def token(self):
         return self._token
 
+    def has_token_type(self, token_type):
+        return self._token.__class__ == token_type
+
+    @property
+    def is_end(self):
+        return self.has_token_type(EndToken)
+
     @property
     def binding_power(self):
-        return self.token.binding_power
+        return self._token.binding_power
 
     @property
     def left(self):
         return self._left
 
-    @property
-    def stream(self):
-        return self._stream
-
     def next(self, left = None):
-        return ParseState(self._stream, left)
+        return ParseState(self._remaining_tokens, left)
+
+    def __repr__(self):
+        return "%s(%r, %r, %r)" % (self.__class__.__name__, self._token, self._remaining_tokens, self.left)
 
 class TokenBindingPower(object):
     LEVEL_1 = 100
@@ -124,6 +131,12 @@ class AbstractToken(object):
     def binding_power(self):
         pass
 
+    def __repr__(self):
+        return "%s()" % self.__class__.__name__
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__
+
 class LeftParenthesisToken(AbstractToken):
     @property
     def binding_power(self):
@@ -131,24 +144,47 @@ class LeftParenthesisToken(AbstractToken):
 
     def create_prefixed_expression(self, parser, state):
         expr = parser.expression(state)
-        parser.advance(state, RightParenthesisToken)
-        return expr
+        if not parser.state.has_token_type(RightParenthesisToken):
+            raise ParsingSyntaxError("Expected %s" % RightParenthesisToken.__name__)
+        
+        # NOTE: parser.expression could do all sorts of parser.state transformations, 
+        # so using state argument from above is really unreliable
+        # TODO: Should be returning this state
+        parser.state = parser.state.next()
+        #print("\nLEFTR")
+        #print(parser.state)
+        #print(state.next().next(expr))
+
+        return state.next(expr)
+
+    def __str__(self):
+        return "("
 
 class RightParenthesisToken(AbstractToken):
     @property
     def binding_power(self):
         return TokenBindingPower.LEVEL_6
 
+    def __str__(self):
+        return ")"
+
 class ConstantToken(AbstractToken):
     def __init__(self, value):
-        self.value = value
+        self._value = value
 
     @property
     def binding_power(self):
         return TokenBindingPower.LEVEL_1
 
+    @property
+    def value(self):
+        return self._value
+
     def create_prefixed_expression(self, parser, state):
-        return SimpleSentence(PropositionalConstant(self.value))
+        return state.next(SimpleSentence(PropositionalConstant(self.value)))
+
+    def __eq__(self, other):
+        return super.__eq__(self, other) and self.value == other.value
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__.__name__, self.value)
@@ -159,7 +195,7 @@ class NegationToken(AbstractToken):
         return  TokenBindingPower.LEVEL_2
 
     def create_prefixed_expression(self, parser, state):
-        return Negation(parser.expression(state, self.binding_power))
+        return state.next(Negation(parser.expression(state, self.binding_power)))
 
 class ConjunctionToken(AbstractToken):
     @property
@@ -185,6 +221,9 @@ class EquivalenceToken(AbstractToken):
     def create_inside_expression(self, parser, state):
         return Equivalence(state.left, parser.expression(state, self.binding_power))
 
+    def __str__(self):
+        return "<=>"
+
 class ImplicationToken(AbstractToken):
     @property
     def binding_power(self):
@@ -192,6 +231,9 @@ class ImplicationToken(AbstractToken):
 
     def create_inside_expression(self, parser, state):
         return Implication(state.left, parser.expression(state, self.binding_power))
+
+    def __str__(self):
+        return "=>"
 
 class ReductionToken(AbstractToken):
     @property
@@ -201,7 +243,13 @@ class ReductionToken(AbstractToken):
     def create_inside_expression(self, parser, state):
         return Reduction(state.left, parser.expression(state, self.binding_power))
 
+    def __str__(self):
+        return "<="
+
 class EndToken(AbstractToken):
     @property
     def binding_power(self):
         return  TokenBindingPower.LEVEL_6
+
+    def __str__(self):
+        return "(end)"
